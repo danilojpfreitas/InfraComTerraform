41daea4e130ecfe9ce0cf0adf28c74066ff9ea01
danilojpfreitas <danilojpfreitas@gmail.com>
2023-05-18 09:00:34 -0300

    Version 1 and Backend S3

---
+++ buckets.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "bucket-stack" {
+  count = length(var.bucket_names)
+  bucket = "${var.project_name}-${var.bucket_names[count.index]}-${var.environment}"
+  force_destroy = true
+
+  tags = {
+    Name        = "${var.project_name}-${var.bucket_names[count.index]}-${var.environment}"
+    Environment = var.environment
+    Cost_Center = "TI"
+    project_name = var.project_name
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket-encryption" {
+  count = length(var.bucket_names)
+  bucket = "${var.project_name}-${var.bucket_names[count.index]}-${var.environment}"
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket-acl" {
+  count = length(var.bucket_names)
+  bucket = "${var.project_name}-${var.bucket_names[count.index]}-${var.environment}"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket-public-1" {
+  count = length(var.bucket_names)
+  bucket = "${var.project_name}-${var.bucket_names[count.index]}-${var.environment}"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
